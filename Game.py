2181189a05eb1bf55d2a934512bd09a2a70d2b00
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2020-11-27 22:18:35 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -4,7 +4,7 @@ import pygame
 from pygame.locals import * # Basic pygame imports
 
 # Global Variables for the game
-FPS = 32
+FPS = 32 
 SCREENWIDTH = 289  
 SCREENHEIGHT = 511
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
